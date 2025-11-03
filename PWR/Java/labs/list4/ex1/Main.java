7bfd05732264ab0949e3e40d3b34810c23c21f5d
Kacper Gęśla <kacper.gesla29@gmail.com>
2025-11-03 18:30:22 +0100

    ex1 list 4

--- PWR/Java/labs/list4/ex1/Main.java
+++ PWR/Java/labs/list4/ex1/Main.java
@@ -4,20 +4,45 @@ import static java.lang.IO.*;  //including package IO to be able to use simple p
 import static java.lang.IO.println;
 import static term.term.*;     //includes package term (createElements() functions were moved there)
 
-int prng(int seed){
-    return (123 * seed + 321) % 10;
+public static class Rand{
+    static long seed = 1234;
+    static long mod = 32;
+    static long c = 143;
+    static long a = 17875;
+
+    public static long random(){
+        long res = (Rand.seed * Rand.a + Rand.c) % mod;
+        seed = res;
+        return res;
+    }
+}
+
+int randomTwoSets(){
+    int[] set = new int[32];
+    int j = 0;
+    for (int i = 10; i <= 20; i++){
+        set[j] = i;
+        j++;
+    }
+    for (int i = 50; i <= 70; i++){
+        set[j] = i;
+        j++;
+    }
+
+
+    int random_num = (int)Rand.random();
+    return set[random_num];
+
 }
 
 
 void main() {
-    int seed = 1234;
 
-    for (int i = 0; i < 20; i ++) {
-        int a = prng(seed);
-        seed = a;
-        System.out.print(a + ", ");
+
+    for (int i = 0; i<30; i++){
+        print(randomTwoSets() + ", ");
     }
-
-
-
+    println("");
+    println("");
+    println("");
 }
